30b5a08cffec984a39541741fb3ff3a0f60ccda5
Orbit Agent <orbit@orbit-ss.dev>
2026-04-07 21:11:27 +0000

    that shit does not load. clicking start does nothugn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["*.vercel.run", "*.vercel.app"],
 };
 
 export default nextConfig;
